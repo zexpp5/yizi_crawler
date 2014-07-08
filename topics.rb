86c00cf5e84964c42d35ba1fd65000cf7864f879
zexpp5 <zexpp5@gmail.com>
2014-07-08 16:10:34 +0800

    revision

--- topics.rb
+++ topics.rb
@@ -47,19 +47,29 @@ for i in (1  .. 30) do
   level = nil
   stage = s[0,2]
   subject = s[2,s.length]
+  parent1 = 0
+  parent2 = 0
+  parent3 = 0
 
   page.css('.fn-clear').each{ |item|
-    item.css('.tree-title1,.tree-title2,.tree-title3').each{ |item1|
+    item.css('.tree-title1,.tree-title2,.tree-title3,.tree-title4').each{ |item1|
       out = ""
       if item1["class"].to_s.index("title1") then  out="-1-" end
       if item1["class"].to_s.index("title2") then  out="-2-" end
       if item1["class"].to_s.index("title3") then  out="-3-" end
+      if item1["class"].to_s.index("title4") then  out="-4-" end
       item1.css(".item").each{|item2|
-        if out.index("-1-") == 0 then parent = item2["data-nselect"]; level = 1 end
-        if out.index("-2-") == 0 then parent = item2["data-nselect"]; level = 2 end
-        if out.index("-3-") == 0 then level = 3 end
+        if out.index("-1-") == 0 then level = 1 end
+        if out.index("-2-") == 0 then level = 2; parent = parent1 end
+        if out.index("-3-") == 0 then level = 3; parent = parent2 end
+        if out.index("-4-") == 0 then level = 4; parent = parent3 end
         t = Topic.new(item2["data-nselect"],item2["title"],level,parent,stage,subject)
         topics[item2["data-nselect"]] = t
+
+        if out.index("-1-") == 0 then parent1 = item2["data-nselect"] end
+        if out.index("-2-") == 0 then parent2 = item2["data-nselect"] end
+        if out.index("-3-") == 0 then parent3 = item2["data-nselect"] end
+
       }
     }
   }
@@ -87,6 +97,7 @@ ensure
 # disconnect from server
   db.close if db
 end
+
 
 
 
